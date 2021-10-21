e4548327a9f3ee089a9a997db0efb02e1006aa80
Gerson Perdomo <ing.gersonp@gmail.com>
2021-10-21 16:09:06 -0500

    Fixing STR error

--- utilities/setter.py
+++ utilities/setter.py
@@ -4,7 +4,7 @@ from base64 import b64decode
 def main():
     key = os.environ.get('SERVICE_ACCOUNT_KEY')
     with open('path.json','w') as json_file:
-        json_file.write(b64decode(key.decode()))
+        json_file.write(b64decode(key).decode())
     print(os.path.realpath('path.json'))
 
 if __name__ == '__main__':
